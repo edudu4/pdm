13db568c094d46a2312304cf86096e982194e3e9
Eduardo Alves <eduardoa8142@gmail.com>
2023-08-29 21:16:16 -0300

    aulao2 - introdução react native

---
+++ aulas/aula02/App.js
@@ -0,0 +1,22 @@
+import { View, StyleSheet } from 'react-native'
+import Box from './components/box'
+// componente principal
+const App = () => (
+  <View style={styles.container}>
+    <Box color={'red'} size={50}></Box>
+    <Box color={'green'} size={50}></Box>
+    <Box color={'blue'} size={50}></Box>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  }
+})
+
+export default App;
